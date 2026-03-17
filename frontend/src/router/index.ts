f8ac1f24b1ad4e1ca466486e363691a65dc9e8c0
Purgato96 <matheus_purgato@hotmail.com>
2026-03-17 10:50:28 -0300

    Add TailwindCSS

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Index from '@/viewes/Index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index,
+      meta: {
+        title: 'Index',
+      },
+    },
+  ],
 })
 
 export default router
