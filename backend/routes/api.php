4fe7685cea08da26cf11282ef8edf6f19503eb06
Purgato96 <matheus_purgato@hotmail.com>
2026-03-24 09:13:37 -0300

    TEST: Test route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Rota de teste para verificar se a API está funcionando
+Route::prefix('v1')->name('api.')->group(function () {
+    Route::get('/test', function () {
+        return response()->json([
+            'message' => 'Hello World! Você obteve um status 200 OK!',
+        ]);
+    }
+    );
+});
